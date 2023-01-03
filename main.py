8059486f3b37a43a671bd008f0c8b2fc1bfae34a
Marwan K <marwan.kaouachi@gmail.com>
2023-01-03 12:58:34 +0100

    Creation of the main file

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+# Authors:  - Marwan Kaouachi
+#           - Nikolas Milovanovic
+#           - Aymen Soltani
+# Start date: 03/01/2023
+# End date:   22/01/2023
+"""
+
+# Dependecies
+import numpy as np
+
+# Functions
+def create_matrix(height, width): # Matrix Creation function
+    return np.zeros((height, width), dtype = np.int32)
+    
